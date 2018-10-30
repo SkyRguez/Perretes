8809453598867df58194c90aab432e94c7465815
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-10-30 14:22:18 +0100

    Extract toJson function

--- index2.js
+++ index2.js
@@ -7,14 +7,16 @@ function start() {
 function onClick() {
   const RANDOM_DOGS_URL = "https://dog.ceo/api/breeds/image/random";
   fetch(RANDOM_DOGS_URL)
-    .then(function(apiResponse) {
-      //console.log(apiResponse.json());
-      return apiResponse.json();
-    })
+    .then(toJson)
     .then(function(jsonResponse) {
       const img = document.createElement("img");
       img.alt = "perrito lindo";
       img.src = jsonResponse.message;
       document.querySelector(".doggos").appendChild(img);
     });
+}
+
+function toJson(apiResponse) {
+  //console.log(apiResponse.json());
+  return apiResponse.json();
 }
