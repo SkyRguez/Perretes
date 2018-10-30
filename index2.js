71ff715e31d0451be6f6ea9c89285626397c36f0
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-10-30 13:40:00 +0100

    Add segunda parte de la promesa

--- index2.js
+++ index2.js
@@ -2,4 +2,8 @@ const RANDOM_DOGS_URL = "https://dog.ceo/api/breeds/image/random";
 fetch(RANDOM_DOGS_URL).then(function(apiResponse) {
   //console.log(apiResponse.json());
   return apiResponse.json();
-});
+})
+.then(function(jsonResponse) {
+  const img = document.createElement("img");
+  img.alt = "perrito lindo";
+  img.src = jsonResponse.message;
