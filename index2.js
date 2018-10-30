f8cb7da5cda09e20287cffc69ea8c76bb2d03ad3
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-10-30 13:42:00 +0100

    Add function para todo

--- index2.js
+++ index2.js
@@ -1,12 +1,16 @@
-const RANDOM_DOGS_URL = "https://dog.ceo/api/breeds/image/random";
-fetch(RANDOM_DOGS_URL)
-  .then(function(apiResponse) {
-    //console.log(apiResponse.json());
-    return apiResponse.json();
-  })
-  .then(function(jsonResponse) {
-    const img = document.createElement("img");
-    img.alt = "perrito lindo";
-    img.src = jsonResponse.message;
-    document.querySelector(".doggos").appendChild(img);
-  });
+document.querySelector(".add-doggo").addEventListener("click", onClick);
+
+function onClick() {
+  const RANDOM_DOGS_URL = "https://dog.ceo/api/breeds/image/random";
+  fetch(RANDOM_DOGS_URL)
+    .then(function(apiResponse) {
+      //console.log(apiResponse.json());
+      return apiResponse.json();
+    })
+    .then(function(jsonResponse) {
+      const img = document.createElement("img");
+      img.alt = "perrito lindo";
+      img.src = jsonResponse.message;
+      document.querySelector(".doggos").appendChild(img);
+    });
+}
