d5a36ffcca31d19254d0a54d1a78f861978b43d4
Sara Rodríguez Moldes <raheld12@gmail.com>
2018-10-31 08:52:46 +0100

    Terminando ejercicio con `toJasonResponse`

--- index2.js
+++ index2.js
@@ -8,15 +8,21 @@ function onClick() {
   const RANDOM_DOGS_URL = "https://dog.ceo/api/breeds/image/random";
   fetch(RANDOM_DOGS_URL)
     .then(toJson)
-    .then(function(jsonResponse) {
-      const img = document.createElement("img");
-      img.alt = "perrito lindo";
-      img.src = jsonResponse.message;
-      document.querySelector(".doggos").appendChild(img);
-    });
+    .then(toJsonResponse);
 }
 
 function toJson(apiResponse) {
   //console.log(apiResponse.json());
   return apiResponse.json();
+}
+
+function toJsonResponse(jsonResponse) {
+  function makeImageFrom(dogUrl) {
+    const image = document.createElement("img");
+    img.alt = "perrito lindo";
+    img.src = dogUrl;
+    return image;
+  }
+  const img = makeImageFrom(jsonResponse.message);
+  document.querySelector(".doggos").appendChild(img);
 }
